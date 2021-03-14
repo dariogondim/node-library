7ea02a0b16aa92067e846be7d78c32ec8e14d095
Dario gondim <cognusdev@gmail.com>
2021-03-14 01:03:13 -0300

    add celebrate errors serialization

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -11,6 +11,7 @@ import cors from 'cors';
 import 'express-async-errors';
 
 import AppError from '@shared/errors/AppError';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -24,6 +25,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
